222ec02cfd9df666204cda9fd300a838d1dcab12
Sam <samuel.canonaco@ontariotechu.net>
2021-03-11 10:28:36 -0500

    Auto stash before merge of "master" and "origin/master"

--- projects/CaRPG/src/Scene2.cpp
+++ projects/CaRPG/src/Scene2.cpp
@@ -775,6 +775,19 @@ void Scene2::Start()
 
 int Scene2::Update()
 {
+
+	AudioEngine& engine = AudioEngine::Instance();
+
+	AudioEvent& ambient = engine.GetEvent("Ambient");
+	AudioBus& musicBus = engine.GetBus("Music");
+
+
+	//get ref listener
+	AudioListener& listener = engine.GetListener();
+	listener.SetPosition(glm::vec3(5, 0, 0));
+
+	engine.Update();
+
 	if (isPaused)
 	{
 		return PausedUpdate();
