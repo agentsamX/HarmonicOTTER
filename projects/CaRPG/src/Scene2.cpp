656e78626ad19e86d6d1a1cc11e7afcc759e2470
Sam <samuel.canonaco@ontariotechu.net>
2021-04-07 21:50:08 -0400

    Cleaning for last stuff

--- projects/CaRPG/src/Scene2.cpp
+++ projects/CaRPG/src/Scene2.cpp
@@ -1493,6 +1493,7 @@ int Scene2::Update()
 	{
 		m_Registry.get<syre::PathAnimator>(m_PCar).IncrementSegment(2);//needs changed
 		m_Registry.get<syre::PathAnimator>(m_enemy).IncrementSegment(2);//needs changed
+		helptog = false;
 		if (PlayerComponent.GetScore() >= EnemyComponent.GetScore())
 		{
 			flatShader->SetUniformMatrix("scale", glm::scale(glm::mat4(1.0f), glm::vec3(0.4f)));
@@ -1720,7 +1721,7 @@ int Scene2::Update()
 
 
 
-
+	
 	if (!manualCamera)
 	{
 		camComponent->SetPosition(m_Registry.get<syre::Transform>(m_PCar).GetPosition() + glm::vec3(1.0f, 5.0f, 5.0f));
